d45b7d439d45a42dac52b2a1af168b1a9651d5ef
FabioSenac10 <fabyokamargo@hotmail.com>
2025-05-05 20:49:46 -0400

    while

--- #while-continue.py
+++
@@ -1,9 +0,0 @@
-# while-continue
-
-while True:
-    valor=int(input("Digite 1 ou 0 para encerrar: "))
-    if valor>=1:
-     continue
-     print("Maior que 1")
-    if valor>1:
-     print("Maior que 1")
---
+++ thiago-lista.py
@@ -0,0 +1,5 @@
+lista=[10,20,30,40,55,66,77,88,99]
+x=0
+while x<len(lista):
+    print(x, lista[x])
+    x+=1
